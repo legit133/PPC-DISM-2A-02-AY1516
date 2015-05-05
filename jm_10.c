f35f3c75e2e8072e81e86706dc2f06acdfd6e414
YagamiSei <royallyzhen@hotmail.com>
2015-05-05 14:32:22 +0800

    Rename and Added .exe
    Rename with group number in file and compiling it into .exe

--- jm_10.c
+++ jm_10.c
@@ -14,17 +14,21 @@ int main(){
 
 	char job;
 	char option[2] = {'e', 'd'};
+	char message[100];
 	
 	// printf("%c\n", option[0]);
 	// printf("%c\n", option[1]);
 
 	do{	
 		printf("Type 'e' to encrypt or 'd' to decrypt: ");
-		scanf(" %c", &job);
+		scanf(" %c", &job); //Space before conversion is to skip leading whitespace and read the first non-whitespace character
 
 		if (job == option[0]){
 			//Jump to Encryption
 			printf("Encryption Initiating\n");
+			printf("Type message you want to encrypt: ");
+			scanf(" %100s", message );
+			printf("Your message is %100s\n", message);
 			break;
 		} else if (job == option[1]){
 			//Jump to Decryption
